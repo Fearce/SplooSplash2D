a06cd2fe1ff1d270bb4a64a231abd678b1c6feb6
Kian Heinsbæk <k.heinsbaek@gmail.com>
2019-03-26 11:28:48 +0100

    UI + point

---
+++ Assets/Scripts/hpManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hpManager : MonoBehaviour
+{
+    public int lifes;
+
+    public Text hpText;
+
+    private void Update()
+    {
+        hpText.text = "HP: " + lifes;
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Cherry")
+        {
+            Destroy(other.gameObject);
+            lifes++;
+            Debug.Log(lifes);
+            hpText.text = "HP: " + lifes;
+        }
+    }
+}
---
+++ Assets/Scripts/pointsManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pointsManager : MonoBehaviour
+{
+    public int points;
+
+    public Text pointText;
+
+    private void Update()
+    {
+        pointText.text = "POINTS: " + points;
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Points")
+        {
+            Destroy(other.gameObject);
+            points++;
+            pointText.text = "POINTS: " + points;
+        }
+    }
+}
