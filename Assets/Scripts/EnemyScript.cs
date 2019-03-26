1d96e310832283d91bbda14df8b7b09ca1bfcdda
Fearce <thekev93@gmail.com>
2019-03-26 15:31:37 +0100

    refactoring & comments

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,80 +1,93 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class EnemyScript : MonoBehaviour
+namespace Assets.Scripts
 {
-
-    public float speed;
-    public float distance;
-    public Splatter splatter;
-
-    public Transform target;
-
-    public Rigidbody2D PlayerBody;
-    public float JumpForce = 8f;
-    public int Health = 100;
-
-    private bool IsJumping;
-
-    private Animator pacAnim;
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Script for all Enemies
+    /// </summary>
+    public class EnemyScript : MonoBehaviour
     {
-        PlayerBody = gameObject.GetComponent<Rigidbody2D>();
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        pacAnim = target.GetComponent<Animator>();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Health <= 0)
+        // Hmm, setter vi nogensinde Speed & Distance? Så vidt jeg kan se er de altid 0
+        public float Speed;
+        public float Distance;
+        
+        public Splatter Splatter;
+        public Transform Target;
+
+        public Rigidbody2D GhostBody;
+        public float JumpForce = 8f;
+        public int Health = 100;
+
+        private bool isJumping;
+
+        private Animator pacAnim;
+        // Start is called before the first frame update
+        void Start()
         {
-            Debug.Log("ded ghost");
-            Vector3 pos = new Vector3(transform.position.x, transform.position.y, -3.29f);
-            Instantiate(BloodSplash, pos, Quaternion.identity);
-            Splatter splatterObj = (Splatter)Instantiate(splatter, pos, Quaternion.identity);
-            Destroy(gameObject);
+            GhostBody = gameObject.GetComponent<Rigidbody2D>();
+            Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+            pacAnim = Target.GetComponent<Animator>();
         }
 
-        gameObject.transform.rotation = new Quaternion();
-        if (GameObject.FindGameObjectWithTag("Player") != null)
+        // Update is called once per frame
+        void Update()
         {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            // If health <= 0 ghost is dead
+            if (Health <= 0)
+            {
+                Debug.Log("ded ghost");
+                Vector3 pos = new Vector3(transform.position.x, transform.position.y, -3.29f);
+                Instantiate(BloodSplash, pos, Quaternion.identity);
+                Splatter splatterObj = (Splatter)Instantiate(Splatter, pos, Quaternion.identity);
+                Destroy(gameObject);
+            }
+
+            // Make sure ghost isn't tilted
+            gameObject.transform.rotation = new Quaternion();
+
+            // Move towards target
+            if (GameObject.FindGameObjectWithTag("Player") != null)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, Target.position, Speed * Time.deltaTime);
+            }
+
+            // Jump if target is above
+            if (Target != null && (Vector2.Distance(transform.position, Target.position) < Distance && isJumping == false))
+            {
+                GhostBody.AddForce(transform.up * JumpForce * 100);
+                isJumping = true;
+            }
         }
 
-        if (target != null && (Vector2.Distance(transform.position, target.position) < distance && IsJumping == false))
+        public GameObject BloodSplash;
+
+        void OnCollisionEnter2D(Collision2D coll)
         {
-            PlayerBody.AddForce(transform.up * JumpForce * 100);
-            IsJumping = true;
+            // Reset isJumping when touching the floor
+            if (coll.gameObject.tag == "Obstacles")
+            {
+                // Debug.Log("touching floor");
+                isJumping = false;
+            }
+
+            // Take damage from bullet
+            if (coll.gameObject.tag.Contains("Bullet"))
+            {
+                Debug.Log("ghost takin dmg!");
+                Health -= coll.gameObject.GetComponent<BulletScript>().Damage;
+            }
+
+            // When colliding with player (redundant?)
+            if (coll.gameObject.tag == "Player")
+            {
+                Debug.Log("Collision with Player");
+                //Destroy(coll.gameObject, 2.1f);
+                //points++;
+                //GameObject.FindGameObjectWithTag("Text").GetComponent<Text>().text = "Score: " + points;
+                //score.text = "Score: " + points;
+            }
+
         }
-    }
-
-    public GameObject BloodSplash;
-
-    void OnCollisionEnter2D(Collision2D coll)
-    {
-        if (coll.gameObject.tag == "Obstacles")
-        {
-            // Debug.Log("touching floor");
-            IsJumping = false;
-        }
-
-        if (coll.gameObject.tag.Contains("Bullet"))
-        {
-            Debug.Log("ghost takin dmg!");
-            Health -= coll.gameObject.GetComponent<BulletScript>().Damage;
-        }
-
-        if (coll.gameObject.tag == "Player")
-        {
-            Debug.Log("Collision with Player");
-            //Destroy(coll.gameObject, 2.1f);
-            //points++;
-            //GameObject.FindGameObjectWithTag("Text").GetComponent<Text>().text = "Score: " + points;
-            //score.text = "Score: " + points;
-        }
-
     }
 }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,22 +4,28 @@ using UnityEngine.UI;
 
 namespace Assets.Scripts
 {
+    /// <summary>
+    /// Player script
+    /// </summary>
     public class PlayerScript : MonoBehaviour
     {
-        public int lifes;
+        public int Lives;
 
-        public Text hpText;
-        public GameObject deadPanel;
+        public Text HpText;
+        public GameObject DeadPanel;
 
+        // StickDirection is set in Joystick.cs on drag
         public static Vector2 StickDirection { get; set; }
         private float StickX => StickDirection.x;
         private float StickY => StickDirection.y;
+
+        // Default speed and jump force
         public float MoveSpeed = 0.05f;
         public float JumpForce = 8f;
+        
+        private bool isJumping;
 
-        private bool IsJumping;
-
-        private bool IsDead;
+        private bool isDead;
 
         private Animator myAnim;
 
@@ -34,6 +40,11 @@ namespace Assets.Scripts
             myAnim = GetComponent<Animator>();
 
         }
+
+        /// <summary>
+        /// Resets isJumping every third second.
+        /// Can be improved upon.
+        /// </summary>
         DateTime lastJump = DateTime.MinValue;
 
         void FixJumpingBug()
@@ -41,7 +52,7 @@ namespace Assets.Scripts
             if (PlayerBody.velocity.y < 1f && (lastJump == DateTime.MinValue || DateTime.UtcNow > lastJump.AddSeconds(3)))
             {
                 lastJump = DateTime.UtcNow;
-                IsJumping = false;
+                isJumping = false;
                 Debug.Log("jump fixed");
             }
         }
@@ -50,20 +61,26 @@ namespace Assets.Scripts
         void Update()
         {
             FixJumpingBug();
-            hpText.text = "HP: " + lifes;
+            HpText.text = "HP: " + Lives;
 
-            if (lifes == 0)
+            // Ded on 0 lives
+            if (Lives == 0)
             {
-                IsDead = true;
-                deadPanel.SetActive(true);
+                isDead = true;
+                DeadPanel.SetActive(true);
             }
 
-            if (!IsDead)
+            // If not ded
+            if (!isDead)
             {
+                // Center camera on player
                 GameObject cam = GameObject.FindGameObjectWithTag("MainCamera");
                 cam.transform.position = new Vector3(Player.position.x, cam.transform.position.y, -10f);
 
+                // Fix rotation so we don't tilt
                 gameObject.transform.rotation = new Quaternion();
+
+                // Movement
                 // Right
                 if (StickX > 0.2 || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
                 {
@@ -78,64 +95,59 @@ namespace Assets.Scripts
                 }
 
                 // Up
-                if ((StickY > 0.5 || (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))) && IsJumping == false )
+                if ((StickY > 0.5 || (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))) && isJumping == false )
                 {
                     PlayerBody.AddForce(transform.up * JumpForce * 100);
-                    IsJumping = true;
+                    isJumping = true;
                     //Player.position = new Vector3(Player.position.x - MoveSpeed, Player.position.y);
                 }
             }
             else
             {
+                // Set death animation
                 myAnim.SetBool("death", true);
             }
         }
 
 
-        // Dummy code for future use (from cloud jumper)
+        // Collision
         void OnCollisionEnter2D(Collision2D coll)
         {
+            // Reset isJumping when touching floor to allow new jump
             if (coll.gameObject.tag == "Obstacles")
             {
                 // Debug.Log("touching floor");
-                IsJumping = false;
+                isJumping = false;
             }
 
+            // On hitting ghost
             if(coll.gameObject.tag == "Enemy")
             {
-                if (lifes > 0)
+                if (Lives > 0)
                 {
-                    lifes--;
+                    Lives--;
                 }
-                Debug.Log(lifes);
-                hpText.text = "HP: " + lifes;
+                Debug.Log(Lives);
+                HpText.text = "HP: " + Lives;
 
-                if(lifes == 0)
+                if(Lives == 0)
                 {
+                    // Destroy game object in 2.1 seconds
                     Destroy(gameObject, 2.1f);
                 }
             
-            }
-
-            if (coll.gameObject.tag == "coin")
-            {
-                Debug.Log("Collision with coin");
-            
-                Destroy(coll.gameObject);
-                //points++;
-                //GameObject.FindGameObjectWithTag("Text").GetComponent<Text>().text = "Score: " + points;
-                //score.text = "Score: " + points;
             }
         }
 
         public void OnTriggerEnter2D(Collider2D other)
         {
+            // Hitting cherries. Should points be in here too instead of separate script?
             if (other.gameObject.tag == "Cherry")
             {
                 Destroy(other.gameObject);
-                lifes++;
-                Debug.Log(lifes);
-                hpText.text = "HP: " + lifes;
+                Lives++;
+                Debug.Log(Lives);
+                HpText.text = "HP: " + Lives;
             }
         }
 
