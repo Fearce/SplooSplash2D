95ed8fdc018a65afccf53ff2dc80494b05f5a2c2
Louis Verond Høgholt <louisvhoegholt@gmail.com>
2019-04-02 13:08:05 +0200

    Removed unused props

--- Assets/Scripts/PortalSpawn.cs
+++ Assets/Scripts/PortalSpawn.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Assets.Scripts;
 using UnityEngine;
 
 public class PortalSpawn : MonoBehaviour
 {
+    public EnemyScript Enemy;
+
+    public Rigidbody2D Rigidbody2D;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
