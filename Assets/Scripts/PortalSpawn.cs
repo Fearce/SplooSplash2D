05440ac62ebf777e95c6e7cdf2715852e5879af2
Louis Verond Høgholt <louisvhoegholt@gmail.com>
2019-04-02 15:12:54 +0200

    Portal animation

--- Assets/Scripts/PortalSpawn.cs
+++ Assets/Scripts/PortalSpawn.cs
@@ -13,23 +13,33 @@ public class PortalSpawn : MonoBehaviour
     public float spawnRate;
 
     public float nextSpawn;
+    private Animator spawnAnim;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("GhostSpawn", spawnRate, nextSpawn);
+        spawnAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     public void GhostSpawn()
     {
-            GameObject ps = GameObject.FindGameObjectWithTag("PortalSpawn");
-            spawnX = ps.transform.position.x;
-            whereEnemySpawn = new Vector2(spawnX, transform.position.y);
-            Instantiate(enemy, whereEnemySpawn, Quaternion.identity);
+        StartCoroutine("SpawnAnim");
+        SpawnAnim();
+    }
+
+    IEnumerator SpawnAnim()
+    {
+        spawnAnim.SetBool("enemy", true);
+        GameObject ps = GameObject.FindGameObjectWithTag("PortalSpawn");
+        spawnX = ps.transform.position.x;
+        whereEnemySpawn = new Vector2(spawnX, transform.position.y);
+        yield return new WaitForSeconds(1f);
+        Instantiate(enemy, whereEnemySpawn, Quaternion.identity);
+        spawnAnim.SetBool("enemy", false);
         if (stopSpawn)
         {
             CancelInvoke("GhostSpawn");
         }
-     
     }
 }
