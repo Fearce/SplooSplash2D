6a9ae44361680a08113f7977061111f90106a27d
Kian Heinsbæk <k.heinsbaek@gmail.com>
2019-04-06 15:15:51 +0200

    Færdig Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject aboutPanel;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void AboutTheGame()
+    {
+        aboutPanel.SetActive(true);
+    }
+
+    public void cancelAboutTheGame()
+    {
+        aboutPanel.SetActive(false);
+    }
+}
